8e1056ae786220c8f17793997d906b750513ae6f
Daniel Lopez <daniellop199731@gmail.com>
2025-04-09 18:26:09 -0500

    Parte 2: Creacion de la clase service
    Las clases service se encargan solo de optener los datos de las clases repositories y aplicar logica de negocio, tambien de manejo de excepciones a la hora de invocar los metodos de las clases repositories.

---
+++ src/main/java/com/daniel/springboot/di/app/springboot_di/services/ProductService.java
@@ -0,0 +1,33 @@
+package com.daniel.springboot.di.app.springboot_di.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.daniel.springboot.di.app.springboot_di.models.Product;
+import com.daniel.springboot.di.app.springboot_di.repositories.ProductRepository;
+
+/**
+ * Clase service que simula la capa de logica de negocio
+ */
+public class ProductService {
+
+    private ProductRepository repository = new ProductRepository();
+    
+    public List<Product> findAll(){
+        
+        /*
+         * Como parte de la simulacion de la logica de negocio
+         * a cada precio de los productos se le multiplica un impuesto del 1.25
+         */
+        return repository.findAll().stream().map(p -> {
+            Double priceImp = p.getProductPrice() * 1.25d;
+            p.setProductPrice(priceImp.longValue());
+            return p;
+        }).collect(Collectors.toList());
+    }
+
+    public Product findById(Long id){
+        return repository.findById(id);
+    }
+
+}
